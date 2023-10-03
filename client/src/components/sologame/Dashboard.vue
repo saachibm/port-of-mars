f4438f7ccebed9e4e6e809822a7cf0421177b6c6
sabrinanel3 <66537526+sabrinanel3@users.noreply.github.com>
2023-10-03 15:38:30 -0700

    fix: layout, text overlap, events

--- client/src/components/sologame/Dashboard.vue
+++ client/src/components/sologame/Dashboard.vue
@@ -21,39 +21,41 @@
       <div class="d-flex flex-column flex-grow-1 overflow-hidden">
         <div class="d-flex flex-md-row flex-column flex-grow-1 overflow-hidden mh-50">
           <div class="cell-grow mw-35">
-            <h4>Thresholds</h4>
-            <Threshold
-              v-if="state.treatmentParams.thresholdInformation !== 'unknown'"
-              :state="state"
-            />
-            <div v-else class="d-flex flex-column h-75 align-items-center justify-content-center">
-              <h1 class="text-danger mb-3"><b-icon icon="eye-slash-fill" /></h1>
-              <h4 class="text-danger display-5">UNAVAILABLE</h4>
-            </div>
-          </div>
-          <div class="d-flex flex-md-column flex-row flex-grow-1 overflow-hidden mw-35">
-            <div class="cell-grow">
-              <div>
-                <h4 class="text-center">Round</h4>
-                <div class="d-flex justify-content-center align-items-center">
-                  <div class="vfd-container p-2">
-                    <VFDNumberDisplay :digits="2" :value="state.round" variant="red" size="2" />
-                  </div>
-                  <h4 v-if="state.treatmentParams.isKnownNumberOfRounds" class="mx-2">/</h4>
-                  <div v-if="state.treatmentParams.isKnownNumberOfRounds" class="vfd-container p-2">
-                    <VFDNumberDisplay :digits="2" :value="state.maxRound" variant="red" size="2" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="cell-grow d-flex flex-column">
-              <h4 class="text-center">Time remaining</h4>
-              <div class="d-flex justify-content-center">
-                <Clock :timeRemaining="state.timeRemaining" :size="3" />
+            <div>
+              <h4>Thresholds</h4>
+              <Threshold
+                v-if="state.treatmentParams.thresholdInformation !== 'unknown'"
+                :state="state"
+              />
+              <div v-else class="d-flex flex-column h-40 align-items-center justify-content-center">
+                <h1 class="text-danger mb-3"><b-icon icon="eye-slash-fill" /></h1>
+                <h4 class="text-danger display-5">UNAVAILABLE</h4>
               </div>
             </div>
           </div>
-          <div class="cell-grow mw-35"></div>
+          <div class="cell-shrink mw-25">
+            <!-- <div class="cell-grow d-flex flex-column"> -->
+            <h4 class="text-center">Time remaining</h4>
+            <div class="d-flex justify-content-center">
+              <Clock :timeRemaining="state.timeRemaining" :size="3" />
+            </div>
+            <!-- </div> -->
+          </div>
+
+          <div class="cell-grow mw-50">
+            <div>
+              <h4 class="text-center">Round</h4>
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+              <div class="vfd-container p-2">
+                <VFDNumberDisplay :digits="2" :value="state.round" variant="red" size="2" />
+              </div>
+              <h4 v-if="state.treatmentParams.isKnownNumberOfRounds" class="mx-2">/</h4>
+              <div v-if="state.treatmentParams.isKnownNumberOfRounds" class="vfd-container p-2">
+                <VFDNumberDisplay :digits="2" :value="state.maxRound" variant="red" size="2" />
+              </div>
+            </div>
+          </div>
         </div>
         <div class="d-flex flex-md-row flex-column flex-grow-1 overflow-hidden mh-50">
           <div class="cell-grow mw-50 d-flex flex-column justify-content-around">
